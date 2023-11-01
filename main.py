c74a598259345241f2556074711d8b546ebe07be
mandra822 <106483834+mandra822@users.noreply.github.com>
2023-11-01 19:21:33 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertexes = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
+
+
+def cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        for vertex in surface:
+            glVertex3fv(vertexes[vertex])
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertexes[vertex])
+    glEnd()
+
+
+def draw_rectangle():
+    glBegin(GL_QUADS)
+    glColor3f(0.0, 0.4, 0.0)  # Kolor czerwony
+    glVertex3f(2.5, -1.75, -2.5)
+    glVertex3f(2.5, -1.75, 2.5)
+    glVertex3f(-2.5, -1.75, 2.5)
+    glVertex3f(-2.5, -1.75, -2.5)
+    glEnd()
+
+
+def sphere():
+    quad = gluNewQuadric()
+    gluQuadricNormals(quad, GLU_SMOOTH)
+    gluQuadricTexture(quad, GL_TRUE)
+
+    glPushMatrix()
+    glColor3f(1.0, 1.0, 0.0)  # Kolor kuli
+    glTranslatef(0, 0, -3)  # Przesunięcie kuli w przestrzeni
+    gluSphere(quad, 0.5, 32, 32)  # Rysowanie kuli
+    glPopMatrix()
+
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0, 0, -8)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(0.5, 0, 1, 0)  # 1szy argument to predkosc
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glClearColor(0.7, 0.7, 1.0, 1.0)  # Tło niebieskie
+        #cube()
+        sphere()
+        draw_rectangle()
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+if __name__ == "__main__":
+    main()
